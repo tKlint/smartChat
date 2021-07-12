8d12bbf6da984870ed05da3135a64ff57e2b583d
kuicodefly <49471670+kuicodefly@users.noreply.github.com>
2021-07-12 16:06:05 +0800

    格式转换

--- util/index.js
+++ util/index.js
@@ -1,73 +1,17 @@
-const express = require('express');
-const path = require("path");
-const fs = require('fs');
-const http = require('http');
-
-const wss = require('./socket');
-const router = require("./routes");
-
-const app = express();
-/**
- * 服务端口
- */
-const port = 3001;
-/**
- * 定义http服务
- */
-const server = http.createServer(app);
-/**
- * 允许访问的静态资源
- */
-app.use('/static', express.static(path.join(__dirname, 'public')));
-/**
- * 允许访问的临时资源
- */
-app.use('/temp', express.static(path.join(__dirname, 'temp')));
 
 /**
- * 访问根目录
- * 响应welcome.html
+ * 解码x-www-form-urlencoded
  */
-app.all('/', async (req, res) => {
-    const welcomePage = await fs.readFileSync(`${__dirname}/public/welcome.html`);
-    // res.setHeader('Content-Type', 'text/html; charset=utf-8');
-    res.send(welcomePage);
-});
-/**
- * 挂载router
- */
-app.use('/api', router);
-/**
- * 404
- */
-app.use(async (req, res) => {
-    const notFoundPage = await fs.readFileSync(`${__dirname}/public/404.html`);
-    res.setHeader('Content-Type', 'text/html; charset=utf-8');
-	res.status(404).send(notFoundPage);
-});
+const deCodeFormUrl = (url) => {
+    const chatAtIdx = url.indexOf('?');
+    const queryString = url.substring(chatAtIdx + 1);
+    const queryGroup = queryString.split('&');
+    const formUrlObject = {};
+    queryGroup.forEach(item => {
+        const keyToValues = item.split("=");
+        formUrlObject[keyToValues[0]] = keyToValues[1];
+    });
+    return formUrlObject;
+}
 
-
-/**
- * 监听upgrade事件
- * 分配socket服务
- */
-server.on('upgrade', function upgrade(request, socket, head) {
-    const charAt = request.url.indexOf('?');
-    // 模拟子路由
-    const pathname = request.url.substring(0, charAt);
-    if (pathname === '/chatting') {
-        wss.handleUpgrade(request, socket, head, function done(conn) {
-            wss.emit('connection', conn, request);
-        });
-    } else {
-        console.log("no this socket server");
-        socket.destroy();
-    }
-});
-
-
-server.listen(port, () => {
-    console.log(`App runing on http://127.0.0.1:${port}`);
-});
-
-// http://127.0.0.1:3001/23123?w=12=3
+module.exports = { deCodeFormUrl }
