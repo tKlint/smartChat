1dbf2625af1d874e569c4030317d1074cfb4233a
kuicodefly <49471670+kuicodefly@users.noreply.github.com>
2021-07-14 17:23:58 +0800

    调整文件结构

---
+++ libs/BaseController.js
@@ -0,0 +1,9 @@
+class BaseController {  
+    resolve(){  
+        return new Proxy(this, {  
+            get(target, name) {  
+                return target[name].bind(target)  
+            }  
+        })  
+    }  
+}
